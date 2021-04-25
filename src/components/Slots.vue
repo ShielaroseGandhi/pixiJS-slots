d20d951f659c6fdbbb0271f29d9c4d0573386eb2
Shielarose Gandhi <shielarosegandhi@gmail.com>
2021-04-25 15:51:19 -0400

    install pixi js and remove default vue content

---
+++ src/components/Slots.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Pixi JS slots</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slots',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
