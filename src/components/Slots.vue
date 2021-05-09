14943f470ba887feef8f5cc6d7f8b6e89d530037
Shielarose Gandhi <shielarosegandhi@gmail.com>
2021-05-09 13:37:44 -0400

    added custom assets

--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -26,6 +26,8 @@
     props: {
     },
     mounted(){
+        const _self = this;
+
         const app = new PIXI.Application({
           backgroundColor: 0xefefef
         });
@@ -45,148 +47,172 @@
           wordWrapWidth: 440,
         });
 
-        // Build the reels
-        // const reels = [];
-        const reelContainer = new PIXI.Container();
-        reelContainer.y = margin / 2;
-        // 3 reels
-        for (let i = 0; i < 3; i++) {
-          const rc = new PIXI.Container();
-          rc.x = i * REEL_WIDTH + (margin / 2);
-          reelContainer.addChild(rc);
+        // Load sprites
+        app.loader
+          .add([
+            "/assets/images/seven-black.PNG",
+            "/assets/images/triple7-yellow.PNG",
+            "/assets/images/seven-text_transparent.PNG",
+            "/assets/images/seven-yellow.PNG",
+            "/assets/images/triple7-black.PNG",
+          ])
+          .load(onAssetsLoaded);
 
-          const reel = {
-            container: rc,
-            symbols: [],
-            position: 0,
-            previousPosition: 0,
-            blur: new PIXI.filters.BlurFilter(),
-          };
-          reel.blur.blurX = 0;
-          reel.blur.blurY = 0;
-          rc.filters = [reel.blur];
+        // onAssetsLoaded handler builds the example.
+        function onAssetsLoaded() {
+          // Create different slot symbols.
+          const slotTextures = [
+            PIXI.Texture.from('/assets/images/seven-text_transparent.PNG'),
+            PIXI.Texture.from('/assets/images/triple7-yellow.PNG'),
+            PIXI.Texture.from('/assets/images/seven-black.PNG'),
+            PIXI.Texture.from('/assets/images/seven-yellow.PNG'),
+            PIXI.Texture.from('/assets/images/triple7-black.PNG'),
+          ];
 
-          // Build the symbols
-          for (let j = 0; j < 4; j++) {
-            const symbol = new PIXI.Text("7", style);
-            // Scale the symbol to fit symbol area.
-            symbol.y = j * SYMBOL_SIZE;
-            symbol.scale.x = symbol.scale.y = Math.min(SYMBOL_SIZE / symbol.width, SYMBOL_SIZE / symbol.height);
-            symbol.x = Math.round((SYMBOL_SIZE - symbol.width + margin / 2) / 2);
-            reel.symbols.push(symbol);
-            rc.addChild(symbol);
-          }
-          this.reels.push(reel);
-        }
-        app.stage.addChild(reelContainer);
+          // Build the reels
+          // const reels = [];
+          const reelContainer = new PIXI.Container();
+          reelContainer.y = margin / 2;
+          // 3 reels
+          for (let i = 0; i < 3; i++) {
+            const rc = new PIXI.Container();
+            rc.x = i * REEL_WIDTH + (margin / 2);
+            reelContainer.addChild(rc);
 
-        //Borders
-        // drawRect(x,y,width,height)
-        const leftBorder = new PIXI.Graphics();
-        leftBorder.beginFill(0, 1);
-        leftBorder.drawRect(0, 0, margin / 2, app.screen.height);
+            const reel = {
+              container: rc,
+              symbols: [],
+              position: 0,
+              previousPosition: 0,
+              blur: new PIXI.filters.BlurFilter(),
+            };
+            reel.blur.blurX = 0;
+            reel.blur.blurY = 0;
+            rc.filters = [reel.blur];
 
-        const rightBorder = new PIXI.Graphics();
-        rightBorder.beginFill(0, 1);
-        rightBorder.drawRect(app.screen.width - margin / 2, 0, margin, app.screen.height);
+            // Build the symbols
+            for (let j = 0; j < 5; j++) {
+              const symbol = new PIXI.Sprite(slotTextures[j]);
+              // Scale the symbol to fit symbol area.
+              symbol.y = j * SYMBOL_SIZE;
+              symbol.scale.x = symbol.scale.y = Math.min(SYMBOL_SIZE / symbol.width, SYMBOL_SIZE / symbol.height);
+              symbol.x = Math.round((SYMBOL_SIZE - symbol.width + margin / 2) / 2);
 
-        const bottomBorder = new PIXI.Graphics();
-        bottomBorder.beginFill(0, 1);
-        bottomBorder.drawRect(0, app.screen.height - margin, app.screen.width, margin);
-
-        app.stage.addChild(leftBorder);
-        app.stage.addChild(bottomBorder);
-
-
-        // Top Border
-        const top = new PIXI.Graphics();
-        top.beginFill(0, 1);
-        top.drawRect(0, 0, app.screen.width, margin);
-
-        // Add Jackpot Text
-        const playText = new PIXI.Text("JACKPOT", style);
-        playText.x = Math.round((top.width - playText.width) / 2);
-        playText.y = Math.round((margin - playText.height) / 2);
-        top.addChild(playText);
-
-        // // Add ClickMe Text
-        // const clickMe = new PIXI.Text("Click Me");
-        // clickMe.style = {fill: "white", fontSize: 40};
-        // clickMe.x = Math.round((top.width - clickMe.height));
-        // clickMe.y = Math.round((margin + clickMe.width));
-        // clickMe.anchor.x = 0.5;
-        // clickMe.anchor.y = 0.5;
-        // clickMe.angle = 90;
-        // rightBorder.addChild(clickMe);
-
-
-        app.stage.addChild(top);
-        app.stage.addChild(rightBorder);
-
-
-        // Set the interactivity.
-        // rightBorder.interactive = true;
-        // rightBorder.buttonMode = true;
-        // rightBorder.addListener('pointerdown', () => {
-        //   this.startPlay();
-        // });
-
-        // Listen for animate update.
-        app.ticker.add((delta) => {
-          // Update the slots.
-          for (let i = 0; i < this.reels.length; i++) {
-            const r = this.reels[i];
-            // Update blur filter y amount based on speed.
-            // This would be better if calculated with time in mind also. Now blur depends on frame rate.
-            r.blur.blurY = (r.position - r.previousPosition) * 8;
-            r.previousPosition = r.position;
-
-            // Update symbol positions on reel.
-            for (let j = 0; j < r.symbols.length; j++) {
-              const s = r.symbols[j];
-              const prevy = s.y;
-              s.y = ((r.position + j) % r.symbols.length) * SYMBOL_SIZE - SYMBOL_SIZE;
-              // if (s.y < 0 && prevy > SYMBOL_SIZE) {
-              //   // Detect going over and swap a texture.
-              //   // This should in proper product be determined from some logical reel.
-              //   s.texture = slotTextures[Math.floor(Math.random() * slotTextures.length)];
-              //   s.scale.x = s.scale.y = Math.min(SYMBOL_SIZE / s.texture.width, SYMBOL_SIZE / s.texture.height);
-              //   s.x = Math.round((SYMBOL_SIZE - s.width) / 2);
-              // }
+              reel.symbols.push(symbol);
+              rc.addChild(symbol);
             }
+            _self.reels.push(reel);
           }
-        });
 
-        // Listen for animate update.
-        app.ticker.add((delta) => {
-          const now = Date.now();
-          const remove = [];
-          for (let i = 0; i < this.tweening.length; i++) {
-            const t = this.tweening[i];
-            const phase = Math.min(1, (now - t.start) / t.time);
+          app.stage.addChild(reelContainer);
 
-            t.object[t.property] = lerp(t.propertyBeginValue, t.target, t.easing(phase));
-            if (t.change) t.change(t);
-            if (phase === 1) {
-              t.object[t.property] = t.target;
-              if (t.complete) t.complete(t);
-              remove.push(t);
+          //Borders
+          // drawRect(x,y,width,height)
+          const leftBorder = new PIXI.Graphics();
+          leftBorder.beginFill(0, 1);
+          leftBorder.drawRect(0, 0, margin / 2, app.screen.height);
+
+          const rightBorder = new PIXI.Graphics();
+          rightBorder.beginFill(0, 1);
+          rightBorder.drawRect(app.screen.width - margin / 2, 0, margin, app.screen.height);
+
+          const bottomBorder = new PIXI.Graphics();
+          bottomBorder.beginFill(0, 1);
+          bottomBorder.drawRect(0, app.screen.height - margin, app.screen.width, margin);
+
+          app.stage.addChild(leftBorder);
+          app.stage.addChild(bottomBorder);
+
+
+          // Top Border
+          const top = new PIXI.Graphics();
+          top.beginFill(0, 1);
+          top.drawRect(0, 0, app.screen.width, margin);
+
+          // Add Jackpot Text
+          const playText = new PIXI.Text("JACKPOT", style);
+          playText.x = Math.round((top.width - playText.width) / 2);
+          playText.y = Math.round((margin - playText.height) / 2);
+          top.addChild(playText);
+
+          // // Add ClickMe Text
+          // const clickMe = new PIXI.Text("Click Me");
+          // clickMe.style = {fill: "white", fontSize: 40};
+          // clickMe.x = Math.round((top.width - clickMe.height));
+          // clickMe.y = Math.round((margin + clickMe.width));
+          // clickMe.anchor.x = 0.5;
+          // clickMe.anchor.y = 0.5;
+          // clickMe.angle = 90;
+          // rightBorder.addChild(clickMe);
+
+
+          app.stage.addChild(top);
+          app.stage.addChild(rightBorder);
+
+
+          // Set the interactivity.
+          // rightBorder.interactive = true;
+          // rightBorder.buttonMode = true;
+          // rightBorder.addListener('pointerdown', () => {
+          //   this.startPlay();
+          // });
+
+          // Listen for animate update.
+          app.ticker.add((delta) => {
+            // Update the slots.
+            for (let i = 0; i < _self.reels.length; i++) {
+              const r = _self.reels[i];
+              // Update blur filter y amount based on speed.
+              // This would be better if calculated with time in mind also. Now blur depends on frame rate.
+              r.blur.blurY = (r.position - r.previousPosition) * 8;
+              r.previousPosition = r.position;
+
+              // Update symbol positions on reel.
+              for (let j = 0; j < r.symbols.length; j++) {
+                const s = r.symbols[j];
+                const prevy = s.y;
+                s.y = ((r.position + j) % r.symbols.length) * SYMBOL_SIZE - SYMBOL_SIZE;
+                // if (s.y < 0 && prevy > SYMBOL_SIZE) {
+                //   // Detect going over and swap a texture.
+                //   // This should in proper product be determined from some logical reel.
+                //   s.texture = slotTextures[Math.floor(Math.random() * slotTextures.length)];
+                //   s.scale.x = s.scale.y = Math.min(SYMBOL_SIZE / s.texture.width, SYMBOL_SIZE / s.texture.height);
+                //   s.x = Math.round((SYMBOL_SIZE - s.width) / 2);
+                // }
+              }
             }
+          });
+
+          // Listen for animate update.
+          app.ticker.add((delta) => {
+            const now = Date.now();
+            const remove = [];
+            for (let i = 0; i < _self.tweening.length; i++) {
+              const t = _self.tweening[i];
+              const phase = Math.min(1, (now - t.start) / t.time);
+
+              t.object[t.property] = lerp(t.propertyBeginValue, t.target, t.easing(phase));
+              if (t.change) t.change(t);
+              if (phase === 1) {
+                t.object[t.property] = t.target;
+                if (t.complete) t.complete(t);
+                remove.push(t);
+              }
+            }
+            for (let i = 0; i < remove.length; i++) {
+              _self.tweening.splice(_self.tweening.indexOf(remove[i]), 1);
+            }
+          });
+
+          // Basic lerp funtion.
+          function lerp(a1, a2, t) {
+            return a1 * (1 - t) + a2 * t;
           }
-          for (let i = 0; i < remove.length; i++) {
-            this.tweening.splice(this.tweening.indexOf(remove[i]), 1);
-          }
-        });
 
-        // Basic lerp funtion.
-        function lerp(a1, a2, t) {
-          return a1 * (1 - t) + a2 * t;
-        }
+          // Backout function from tweenjs.
+          // https://github.com/CreateJS/TweenJS/blob/master/src/tweenjs/Ease.js
 
-        // Backout function from tweenjs.
-        // https://github.com/CreateJS/TweenJS/blob/master/src/tweenjs/Ease.js
-
-
+        } //END ONASSETSLOADED FUNCTION
     },
     methods: {
       // Function to start playing.
@@ -228,6 +254,10 @@
       },
       backout(amount) {
         return (t) => (--t * t * ((amount + 1) * t + amount) + 1);
+      },
+      update() {
+        requestAnimationFrame(update);
+        renderer.render(stage);
       }
     }
   }
@@ -259,7 +289,7 @@
     top: 0;
     left: 0;
     transform: translate(-33%, -50%);
-    background-color: rgb(231, 43, 49);
+    background-color: #FFCA05;
     box-shadow: inset -25px -15px 40px rgba(0,0,0,.3);
     background-image: linear-gradient(-45deg, rgba(255,255,220,.3) 0%, transparent 100%);
   }
